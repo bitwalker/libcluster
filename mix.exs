df431788f857a02e0a35fedc1cd0296e4973c832
Paul Schoenfelder <paulschoenfelder@gmail.com>
2017-08-02 11:42:30 -0500

    Version 2.2.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Cluster.Mixfile do
 
   def project do
     [app: :libcluster,
-     version: "2.2.1",
+     version: "2.2.2",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
