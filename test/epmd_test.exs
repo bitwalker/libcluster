b8201c47b4305e1c06b2b0e0e5b426dc21b6443c
Paul Schoenfelder <paulschoenfelder@gmail.com>
2018-02-23 18:59:06 -0600

    Fix epmd test

--- test/epmd_test.exs
+++ test/epmd_test.exs
@@ -4,24 +4,20 @@ defmodule Cluster.Strategy.EpmdTest do
   use ExUnit.Case
 
   alias Cluster.Strategy.Epmd
-  alias Cluster.Nodes
 
   import ExUnit.CaptureLog
 
   describe "start_link/1" do
     test "calls right functions" do
       capture_log(fn ->
-        start_supervised(
-          {Epmd,
-           [
+        :ignore = Epmd.start_link([
              topology: :name,
-             config: [hosts: [:foo@bar]],
-             connect: {Nodes, :connect, [self()]},
-             list_nodes: {Nodes, :list_nodes, [[]]}
-           ]}
-        )
+             config: [hosts: [:"foo@bar"]],
+             connect: {Cluster.Nodes, :connect, [self()]},
+             list_nodes: {Cluster.Nodes, :list_nodes, [[]]}
+        ])
 
-        assert_receive {:connect, :foo@bar}
+        assert_receive {:connect, :"foo@bar"}, 5_000
       end)
     end
   end
