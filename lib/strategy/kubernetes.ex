331854f07f13fc26e4bf2277249a41d8dc3bb8b7
Paul Schoenfelder <paulschoenfelder@gmail.com>
2018-04-04 14:13:50 -0500

    Fix compile warning in Kubernetes strategy

--- lib/strategy/kubernetes.ex
+++ lib/strategy/kubernetes.ex
@@ -153,15 +153,18 @@ defmodule Cluster.Strategy.Kubernetes do
     end
   end
 
-  @env Mix.env()
   @spec get_namespace(String.t()) :: String.t()
-  defp get_namespace(service_account_path) do
-    path = Path.join(service_account_path, "namespace")
+  if Mix.env == :test do
+    defp get_namespace(_service_account_path), do: "__libcluster_test"
+  else
+    defp get_namespace(service_account_path) do
+      path = Path.join(service_account_path, "namespace")
 
-    case File.exists?(path) do
-      true -> path |> File.read!() |> String.trim()
-      false when @env == :test -> "__libcluster_test"
-      false -> ""
+      if File.exists?(path) do
+        path |> File.read!() |> String.trim()
+      else
+        ""
+      end
     end
   end
 
