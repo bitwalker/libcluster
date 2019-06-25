3764b60e275fb016800b5c894f49f2f79e49fae6
Paul Schoenfelder <paulschoenfelder@gmail.com>
2019-06-25 11:36:13 -0400

    Version 3.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Cluster.Mixfile do
 
   def project do
     [app: :libcluster,
-     version: "3.0.3",
+     version: "3.1.0",
      elixir: "~> 1.5",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
