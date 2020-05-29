a9a80048231640cbfc6bda1669b7e87a9a5a92d0
Anderson Bravalheri <andersonbravalheri@gmail.com>
2020-05-29 19:46:00 +0100

    Add more debugging messages
    Although errors and warnings are being logged, some times the strategies
    result in empty sets of nodes. Being able to see more information in the
    logs is helpful to troubleshoot complex cluster systems.

--- lib/strategy/kubernetes_dns.ex
+++ lib/strategy/kubernetes_dns.ex
@@ -117,6 +117,7 @@ defmodule Cluster.Strategy.Kubernetes.DNS do
         case resolver.(headless_service) do
           {:ok, {:hostent, _fqdn, [], :inet, _value, addresses}} ->
             parse_response(addresses, app_name)
+            |> debug_inspect(topology, label: "node names", pretty: true, verbose: 2)
 
           {:error, reason} ->
             error(topology, "lookup against #{service} failed: #{inspect(reason)}")
