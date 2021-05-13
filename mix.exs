b5b2f068f1ff6e715a99748ff1b37d5fce74dc2d
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2021-05-13 11:50:17 -0400

    release: v3.3.0

--- mix.exs
+++ mix.exs
@@ -1,14 +1,14 @@
 defmodule Cluster.Mixfile do
   use Mix.Project
 
-  @version "3.2.2"
+  @version "3.3.0"
   @source_url "https://github.com/bitwalker/libcluster"
 
   def project do
     [
       app: :libcluster,
       version: @version,
-      elixir: "~> 1.5",
+      elixir: "~> 1.8",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       description: """
